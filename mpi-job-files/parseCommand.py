4b09f195806c24de82af72843b380a11cc77cfdb
jacekzbigniewdudek <jacek@pollux.carmichael>
2023-11-20 12:48:48 -0500

    Updated parsing script and manifest template so that they are consistent with recent changes proposed in aaw-kubeflow-containers

--- mpi-job-files/parseCommand.py
+++ mpi-job-files/parseCommand.py
@@ -7,66 +7,81 @@ from pathlib import Path
 # Working directory is assumed to be: 
 # /opt/openmpp/<openmpp-root-dir>/
 
-# Directory where model executables are stored:
-modelsDir = os.path.join(Path.home(), "buckets/aaw-unclassified")
+# Directory used for some start-up options:
+# /etc/openmpp/
 
+# Get directory where model executables are stored:
+#modelsDir = os.path.join(Path.home(), "buckets/aaw-unclassified")
+with open("/etc/openmpp/oms_model_dir") as mD:
+  modelsDir = os.path.join(Path.home(), mD.read())
+
+# Load manifest template contents:
+# Note this one is from <root openmpp dir>/etc
 with open("./etc/MPIJobTemplate.yaml") as template:
   manifest = template.read()
 
+# Save unrecognized command line options for debugging:
 unrecognized = ""
 
+# Some manifest values are based on system configuration:
+# Set model directory:
+manifest = manifest.replace("#<mpirunOption>", \
+  f"- -wdir\n{12*' '}- {modelsDir}\n{12*' '}#<mpirunOption>")
+
+# The remaining manifest values come from command line options passed by oms:
 i = 1
 while i < len(sys.argv):
   if (i + 1 < len(sys.argv) and re.match("^-modelName$", sys.argv[i])):
     manifest = manifest.replace("#<mpiJobName>", f"{sys.argv[i+1]}-{time()}".lower())
     i += 2
 
+  # Number of replicas to create:
   elif (i + 1 < len(sys.argv) and re.match("^-n$", sys.argv[i]) and re.match("^[0-9]+$", sys.argv[i+1])):
-    #print("Found the number of replicas option and argument.")
     manifest = manifest.replace("#<numberOfReplicas>", f"{sys.argv[i+1]}")
     manifest = manifest.replace("#<mpirunOption>", \
       f"- -n\n{12*' '}- '{sys.argv[i+1]}'\n{12*' '}#<mpirunOption>")
     i += 2
 
+  # mpirun bind-to option:
   elif (i + 1 < len(sys.argv) and re.match("^--bind-to$", sys.argv[i]) \
   and re.match("^(core|socket|none)$", sys.argv[i+1])):
-    #print("Found the bind-to option and argument.")
     manifest = manifest.replace("#<mpirunOption>", \
       f"- --bind-to\n{12*' '}- {sys.argv[i+1]}\n{12*' '}#<mpirunOption>")
     i += 2
   
+  # Working directory for mpirun:
   elif (i + 1 < len(sys.argv) and re.match("^-wdir$", sys.argv[i]) and os.path.isdir(sys.argv[i+1])):
-    #print("Found the working directory option and valid path.")
-    #manifest = manifest.replace("#<mpirunOption>", \
-    #  f"- -wdir\n{12*' '}- {sys.argv[i+1]}\n{12*' '}#<mpirunOption>")
-    # For now the working directory is hard-coded so ignore this option.
+    # Ignoring values for this option provided at command line.
     i += 2
 
+  # mpirun environment variable options:
   elif (i + 1 < len(sys.argv) and re.match("^-x$", sys.argv[i]) \
   and re.match("[a-zA-Z_][a-zA-Z0-9_]*=[^=]+$", sys.argv[i+1])):
     manifest = manifest.replace("#<mpirunOption>", \
       f"- -x\n{12*' '}- {sys.argv[i+1]}\n{12*' '}#<mpirunOption>")
-    #print("Found mpirun environment variable option.")
     i += 2
 
+  # Model executable name:
   elif (i < len(sys.argv) and os.path.isfile(os.path.join(modelsDir, sys.argv[i])) \
   and re.match(".*_mpi$", sys.argv[i])):
-    #print("Found model executable name.")
     manifest = manifest.replace("#<modelExecutable>", f"- {os.path.join(modelsDir, sys.argv[i])}")
     i += 1
 
+  # OpenM options and arguments:
   elif (i + 1 < len(sys.argv) and re.match("^-OpenM\.", sys.argv[i]) \
   and re.match("[a-zA-Z0-9_/\.-]+", sys.argv[i+1])):
-    #print("Found an OpenM option and argument.")
     manifest = manifest.replace("#<OpenMOption>", \
       f"- {sys.argv[i]}\n{12*' '}- '{sys.argv[i+1]}'\n{12*' '}#<OpenMOption>") 
     i += 2
 
+  # Unrecognized command line options:
   else:
     unrecognized += f"Unrecognized option or argument: {sys.argv[i]}"
     i += 1
 
+# Write any unrecognized options to file for debugging:
 with open("./etc/unrecognized", "w") as u:
   u.write(unrecognized)
 
+# Print manifest to standard output:
 print(manifest)
