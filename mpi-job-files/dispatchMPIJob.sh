b9818ef331f1cb6bfbbeecabc72fd4872622b1c3
jacekzbigniewdudek <jacek@pollux.carmichael>
2023-12-04 22:21:35 -0500

    Changes to get mpijobs functional.

--- mpi-job-files/dispatchMPIJob.sh
+++ mpi-job-files/dispatchMPIJob.sh
@@ -14,3 +14,17 @@ kubectl apply -f - <<< "$manifest"
 # Now we'd like to run some kind of job monitoring command that returns success when MPIJob is done.
 #kubectl wait --for=condition=complete job/MPIJob
 
+# * Need mpijobname as shell variable. *
+
+# Will try a polling loop with kubectl describe and grepping for relevant parts.
+# The terminating condition will be status succeeded or failed.
+while [condition];
+do
+  foo = $(kubectl describe mpijobs/"$mpijobname" | grep foo) 
+  bar = $(kubectl describe mpijobs/"$mpijobname" | grep bar)
+  baz = $(kubectl describe mpijobs/"$mpijobname" | grep baz)
+
+  # Echo some output based on foo bar baz so that it's routed to openmpp UI.
+
+  # Wait a short interval
+done
