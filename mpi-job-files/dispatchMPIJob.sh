7db28c8ff0edf1719d53cb8acb9c0f89fd3b1b82
jacekzbigniewdudek <jacek@pollux.carmichael>
2023-12-08 00:47:46 -0500

    Another mpijob polling attempt using comm and grep for assistance.

--- mpi-job-files/dispatchMPIJob.sh
+++ mpi-job-files/dispatchMPIJob.sh
@@ -5,6 +5,7 @@
 # Parse openm web service arguments and create manifest instance:
 manifest=$(python3 ./bin/parseCommand.py "$@")
 
+echo "Input Arguments:"
 cat ./etc/inputArguments
 
 # Create a copy for trouble-shooting:
@@ -16,15 +17,28 @@ kubectl apply -f - <<< "$manifest"
 # Now we'd like to run some kind of job monitoring command that returns success when MPIJob is done.
 # Will try a polling loop with kubectl describe and grepping for relevant parts.
 # The terminating condition will be status succeeded or failed.
-mpiJobStatus=""
 mpiJobName=$(<./etc/mpiJobName)
+cat "Pending" > ./etc/mpiJobStatusA
+touch ./etc/mpiJobStatusB
+mpiJobStatus="pending"
 
-while [[ "$mpiJobStatus" != *"Succeeded"* && "$mpiJobStatus" != *"Failed"* ]]; do
+while [[ "$mpiJobStatus" != "done" ]]; do
   # Echo a simple status update. We can elaborate on this later.
-  mpiJobStatus=$(kubectl describe mpijobs/"$mpiJobName" | grep status) 
-  echo $mpiJobStatus
-  echo ". . ."
+  kubectl describe mpijobs/"$mpiJobName" | grep "Message" > ./etc/mpiJobStatusB 
 
-  # Wait a short interval between updates to UI.
-  sleep 2
+  # Output only newest messages:
+  comm -13 ./etc/mpiJobStatusA ./etc/mpiJobStatusB
+ 
+  # Copy over newest changes to old file:
+  cp -f ./etc/mpiJobStatusB ./etc/mpiJobStatusA
+
+  if [[ $(cat ./etc/mpiJobStatusB | grep "JobSucceeded") != "" ]]
+    mpiJobStatus="done"
+  fi
+
+  if [[ $(cat ./etc/mpiJobStatusB | grep "JobSucceeded") == "" ]]
+    # Wait a short interval between updates to UI.
+    echo ". . ."
+    sleep 5
+  fi
 done
