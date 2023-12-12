df08e71caeee72f323a971101d434f3cb54e8e39
jacekzbigniewdudek <jacek@pollux.carmichael>
2023-12-11 23:04:44 -0500

    Trying an approach for monitoring mpijob with a real-time version of logs command.

--- mpi-job-files/dispatchMPIJob.sh
+++ mpi-job-files/dispatchMPIJob.sh
@@ -14,33 +14,6 @@ echo "$manifest" > ./etc/temp.yaml
 # Send manifest to standard input of kubectl:
 kubectl apply -f - <<< "$manifest"
 
-# Now we'd like to run some kind of job monitoring command that returns success when MPIJob is done.
-# Will try a polling loop with kubectl describe and grepping for relevant parts.
-# The terminating condition will be status succeeded or failed.
+wait 5
 mpiJobName=$(<./etc/mpiJobName)
-
-touch ./etc/mpiJobStatusA
-touch ./etc/mpiJobStatusB
-
-cat "Pending" > ./etc/mpiJobStatusA
-mpiJobStatus="pending"
-
-while [[ "$mpiJobStatus" != "done" ]]; do
-  # Grep for status messages regarding current mpijob displayed by kubectl: 
-  kubectl describe mpijobs/"$mpiJobName" | grep "Message:" > ./etc/mpiJobStatusB 
-
-  # Output only newest messages:
-  comm -13 ./etc/mpiJobStatusA ./etc/mpiJobStatusB
- 
-  # Copy over newest changes to old file:
-  cp -f ./etc/mpiJobStatusB ./etc/mpiJobStatusA
-                                                                
-  if [[ $(cat ./etc/mpiJobStatusB | grep "JobSucceeded") != "" \
-     || $(cat ./etc/mpiJobStatusB | grep "JobFailed") != "" ]]; then
-    mpiJobStatus="done"
-  else
-    # Wait a short interval between updates to UI:
-    echo ". . ."
-    sleep 5
-  fi
-done
+kubectl logs -f "$mpiJobName-launcher"
