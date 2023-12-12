017c618dd1eb1245d5c0ccd13670d1c6c7f07654
jacek-dudek <129900469+jacek-dudek@users.noreply.github.com>
2023-12-12 15:33:10 -0500

    Openmpp 14 (#33)
    * Changes to get mpijobs functional.
    
    * Replacing placeholders for the mpijob polling in shell script with basic functionality. Add related change to parseCommand.py to get mpijob name.
    
    * Fixed silly typo in mpi.kubeflow.template.txt
    
    * Tryng another layout of arguments with the mpi.kubeflow template as the previous was not being displayed in console.
    
    * Working changes to all three mpi related files to get mpijob monitoring working.
    
    * Silly typo in mpi.kubeflow template.
    
    * Fixed arguments to String.join() call.
    
    * Error in while loop and wrong file name reference in bash script.
    
    * Fix for a typo that managed to crash the training operator.
    
    * Change in string formatting that was mis-resolving model filename.
    
    * Another mpijob polling attempt using comm and grep for assistance.
    
    * Another change to shell script to fix if syntax error.
    
    * Tidy up presenting output from kubectl describe mpijob to openm UY. Have it report final status if job succeeds or fails.
    
    * Fixing typo in bash script.
    
    * Trying an approach for monitoring mpijob with a real-time version of logs command.
    
    * Used wait instead of sleep.
    
    * Add a check to make sure mpijob launcher pod is running before asking for logs.
    
    * Troubleshooting shell loop.
    
    * Troubleshooting mpijob monitoring via kubectl logs
    
    * Troubleshooting bash script.
    
    * Troubleshooting bash script 2.
    
    * Troubleshooting bash script used for mpijob monitoring.
    
    * Added another pod status to test for in pod status polling loop. Cleared outdated comments and commented out code. Echoing final pod status before going to the logs.
    
    * Fix line continuation syntax error in bash script.
    
    ---------
    
    Co-authored-by: jacekzbigniewdudek <jacek@pollux.carmichael>

--- mpi-job-files/dispatchMPIJob.sh
+++ mpi-job-files/dispatchMPIJob.sh
@@ -5,12 +5,28 @@
 # Parse openm web service arguments and create manifest instance:
 manifest=$(python3 ./bin/parseCommand.py "$@")
 
-# Create a copy for trouble-shooting:
+# Create a copy of manifest for trouble-shooting:
 echo "$manifest" > ./etc/temp.yaml
 
 # Send manifest to standard input of kubectl:
 kubectl apply -f - <<< "$manifest"
 
-# Now we'd like to run some kind of job monitoring command that returns success when MPIJob is done.
-#kubectl wait --for=condition=complete job/MPIJob
+# Set up variables to check mpijob launcher pod status:
+mpiJobName=$(<./etc/mpiJobName)
+podStatus=""
 
+# Poll for status of mpijob launcher pod until it is running or stopped:
+while [[ $podStatus != *"Running"* && $podStatus != *"Completed"* && $podStatus != *"Error"* ]]; do
+  podStatus=$(kubectl get pods | grep "$mpiJobName-launcher")
+  echo "$podStatus"
+  sleep 1
+done
+
+# Show final pod status before going into logs:
+kubectl get pods | grep "$mpiJobName-launcher"
+
+# Forward logs command output from launcher pod:
+kubectl logs -f "$mpiJobName-launcher"
+
+# Enable this after testing works out.
+# kubectl delete "mpijobs/$mpiJobName"
