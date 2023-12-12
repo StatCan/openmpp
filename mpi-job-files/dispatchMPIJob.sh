bdf827fe714dd46fc53409eb0be6ad6491740fa8
jacekzbigniewdudek <jacek@pollux.carmichael>
2023-12-12 13:08:56 -0500

    Added another pod status to test for in pod status polling loop. Cleared outdated comments and commented out code. Echoing final pod status before going to the logs.

--- mpi-job-files/dispatchMPIJob.sh
+++ mpi-job-files/dispatchMPIJob.sh
@@ -5,24 +5,29 @@
 # Parse openm web service arguments and create manifest instance:
 manifest=$(python3 ./bin/parseCommand.py "$@")
 
-# echo "Input Arguments:"
-# cat ./etc/inputArguments
-
-# Create a copy for trouble-shooting:
+# Create a copy of manifest for trouble-shooting:
 echo "$manifest" > ./etc/temp.yaml
 
 # Send manifest to standard input of kubectl:
 kubectl apply -f - <<< "$manifest"
 
+# Set up variables to check mpijob launcher pod status:
 mpiJobName=$(<./etc/mpiJobName)
 podStatus=""
 
-while [[ $podStatus != *"Running"* && $podStatus != *"Completed"* ]]; do
+# Poll for status of mpijob launcher pod until it is running or stopped:
+while [[ $podStatus != *"Running"* \ 
+  && $podStatus != *"Completed"* \
+  && $podStatus != *"Error"* ]]; do
   podStatus=$(kubectl get pods | grep "$mpiJobName-launcher")
   echo "$podStatus"
   sleep 1
 done
 
+# Show final pod status before going into logs:
+kubectl get pods | grep "$mpiJobName-launcher"
+
+# Forward logs command output from launcher pod:
 kubectl logs -f "$mpiJobName-launcher"
 
 # Enable this after testing works out.
