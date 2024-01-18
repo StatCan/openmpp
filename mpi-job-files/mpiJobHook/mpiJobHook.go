4a53ef3eed04efeed633c520813c096035c8629d
jacekzbigniewdudek <jacek@pollux.carmichael>
2024-01-18 09:17:34 -0500

    Work in progress on mpijob handler.

--- mpi-job-files/mpiJobHook/mpiJobHook.go
+++ mpi-job-files/mpiJobHook/mpiJobHook.go
@@ -1,32 +1,116 @@
-// A hook to be used by the openm web service application to call the kubernetes api and submit an mpijob.
-// package mpiJobHook
-// 
-// import (
-//   "fmt"
-// )
+// A handler for mpi-based run requests to be called by the openm web service application. 
+// Accesses the kubernetes api endpoint for mpijobs, requests new mpijob, relays log information from the
+// launcher pod as the job is running, and responds to an abort request if it is relayed by the web service.
 
-package main
+// Rename package name to mpijob after we test the handler functionality, and remove the test harness code.
+package main //mpiJob
 
 import (
-	"context"
-	"fmt"
-	"time"
-
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	//
-	// Uncomment to load all auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth"
-	//
-	// Or uncomment to load specific auth plugins
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+    "fmt",
+    "context",
+	"k8s.io/client-go/rest",
+	"k8s.io/client-go/kubernetes",
+    core "k8s.io/api/core/v1",
+    meta "k8s.io/apimachinery/pkg/apis/meta/v1",
+    kubeflow "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v1"
 )
 
-func main() {
+// Some other imports from go-client examples. 
+// We'll add to imports block if/when need anything from these.
+// "time"
+// "k8s.io/apimachinery/pkg/api/errors"
+// _ "k8s.io/client-go/plugin/pkg/client/auth"
+// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+
+// Will make a simple test harness for the handler and exploration of the rest apis.
+// Check if there are any mandatory arguments for main functions.
+func main () {
+    // Invoke Handler in the same way that oms invokes the goroutine for the ExecCommand thing.
+    go Handler(/* Could use hard-coded arguments for now */)
+
+    // We could pass a unidirectional signal argument as an additional argument and use it to
+    // signal an abort command if/when required.
+    // Use standard input to send signal to terminate.
+}
+
+// Arguments currently passed via the kubeflow template:
+//  ModelName string            // model name
+//  ExeStem   string            // base part of model exe name, usually modelName
+//  Dir       string            // work directory to run the model
+//  BinDir    string            // bin directory where model exe is located
+//  DbPath    string            // absolute path to sqlite database file: models/bin/model.sqlite
+//  MpiNp     int               // number of MPI processes
+//  HostFile  string            // if not empty then path to hostfile
+//  Args      []string          // model command line arguments
+//  Env       map[string]string // environment variables to run the model  
+
+func Handler (modelName, exeStem, dir, binDir, dbPath, hostFile string, mpiNp int, args []string, env map[string]string) (/*no obvious return value*/) {
+
+    // Let's walk down the structure hierarchy for an mpijob specification: 
+    var mpiJobSpec kubeflow.MPIJobSpec
+
+    // probably 2 given Pat's comments about the typical nodes used in the cluster:
+    var slotsPerWorker *int32
+
+    var cleanPodPolicy *CleanPodPolicy
+
+    // This one is a map from the set of replica types {Launcher, Worker, ... } to ReplicaSpecs.
+    // We don't need to provide ReplicaSpecs for any replica types other than Launcher and Worker.
+    // The reason they have these other ones is for specifying different types of distributed workloads.
+    var MPIReplicaSpecs map[kubeflow.ReplicaType]kubeflow.ReplicaSpec
+
+    var mainContainer string
+    var runPolicy kubeflow.RunPolicy
+
+    var launcherReplicaSpec, workerReplicaSpec kubeflow.ReplicaSpec
+
+    var launcherReplicasNumber, workerReplicasNumber *int32
+    var launcherPodTemplateSpec, workerPodTemplateSpec core.PodTemplateSpec
+
+    var launcherMetadata, workerMetadata core.
+    var launcherPodSpec, workerPodSpec core.PodSpec
+
+    var mpiJobContainer core.Container
+    var containerName string
+    var containerImage string
+    var containerCommand []string
+    var containerArguments []string
+    var workingDirectory
+
+    var resourceRequirements core.ResourceRequirements
+    var limits, requests core.ResourceList
+
+    var RestartPolicy
+
+    //Maybe these?
+    var HostName
+    var ServiceAccountName
+    var NodeSelector
+
+
+    // So start off by constructing the container parts.
+    timeStamp := time.Now().UnixNano()
+    containerImage := "k8scc01covidacr.azurecr.io/ompp-run-ubuntu:0f80cf47bb5d6f6e025990e511b72be614bbcf7c"
+
+    containerCommand := string[]{
+
+    mpiJobLauncherContainer = {
+        Name: modelName + "-" + timeStamp + "launcher"
+        Image: containerImage
+
+    }
+
+    mpiJobWorkerContainer = {
+        Name: modelName + "-" timeStamp + "-" + "worker"
+        Image: containerImage
+    }
+
+    
+}
+
+func notMain () {
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
