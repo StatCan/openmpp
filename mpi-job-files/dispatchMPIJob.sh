c696df9ad36fc1d0afc059703554fdf72d5b860e
KrisWilliamson <143441013+KrisWilliamson@users.noreply.github.com>
2024-02-12 15:57:05 -0500

    Update dispatchMPIJob.sh
    Fixed cut-n-paste error.

--- mpi-job-files/dispatchMPIJob.sh
+++ mpi-job-files/dispatchMPIJob.sh
@@ -3,7 +3,7 @@
 # Working directory is assumed to be: /opt/openmpp/<openmpp-root-dir>/
 
 # Parse openm web service arguments and create manifest instance:
-manifest=$(python3 ./parseCommand02.py "$@")
+manifest=$(python3 ./parseCommand.py "$@")
 
 # KLW 16-01-2024 https://github.com/StatCan/openmpp/issues/51
 if [ $? -ne 0 ]; then
