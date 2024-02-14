ff301ffeed0ff3abf8c66726776a050a6866ed96
jacekzbigniewdudek <jacek@pollux.carmichael>
2024-02-13 23:22:01 -0500

    Added code that interacts with kubeflow client. Some build errors remain.

--- mpi-job-files/mpiJob/mpiJob.go
+++ mpi-job-files/mpiJob/mpiJob.go
@@ -9,20 +9,19 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"reflect"
 	"strconv"
 	"strings"
 	"time"
 
 	core "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	//"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	//"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
 	kubeAPI "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v1"
-    kubeClient "github.com/kubeflow/training-operator/pkg/client/clientset/versioned/typed/kubeflow.org/v1"
+	kubeClient "github.com/kubeflow/training-operator/pkg/client/clientset/versioned/typed/kubeflow.org/v1"
 )
 
 // Arguments that are currently passed via the kubeflow template:
@@ -37,7 +36,7 @@ import (
 //  Env       map[string]string // environment variables to run the model
 
 func main() {
-	// Set some sample argument values here.
+	// Set some sample argument values.
 	modelName := "RiskPaths"
 	exeStem := "RiskPaths"
 	dir := "/home/jovyan/buckets/aaw-unclassified/microsim/models/bin"
@@ -45,18 +44,18 @@ func main() {
 	dbPath := "/home/jovyan/buckets/aaw-unclassified/microsim/models/bin/RiskPaths.sqlite"
 	var mpiNp int32 = 4
 	args := []string{
-        "-OpenM.RunStamp",
-        "2024_02_09_04_08_45_834",
-        "-OpenM.LogToConsole",
-        "true",
-        "-OpenM.LogRank",
-        "true",
-        "-OpenM.MessageLanguage",
-        "en-US",
-        "-OpenM.RunName",
-        "RiskPaths_Default_2024_02_08_23_08_10_047",
-        "-OpenM.SetName"
-        "Default"
+		"-OpenM.RunStamp",
+		"2024_02_09_04_08_45_834",
+		"-OpenM.LogToConsole",
+		"true",
+		"-OpenM.LogRank",
+		"true",
+		"-OpenM.MessageLanguage",
+		"en-US",
+		"-OpenM.RunName",
+		"RiskPaths_Default_2024_02_08_23_08_10_047",
+		"-OpenM.SetName",
+		"Default",
 		"-OpenM.SubValues",
 		"32",
 		"-OpenM.Threads",
@@ -66,63 +65,78 @@ func main() {
 		"SAMPLE_ENV": "VALUE",
 	}
 
-	// Create an MPIJobSpec using the sample arguments defined above.
-	jobSpec := mpiJobSpec(modelName, exeStem, dir, binDir, dbPath, mpiNp, args, env)
+	// Create an MPIJob object using the sample arguments defined above.
+	job := mpiJob(modelName, exeStem, dir, binDir, dbPath, mpiNp, args, env)
 
-    // Validate spec using validation function.
-    err := kubeAPI.ValidateV1MpiJobSpec(jobSpec)
-    if err != nil {
-        panic(err.Error())
-    }
+	// Validate spec using validation function.
+	err := kubeAPI.ValidateV1MpiJobSpec(&job.Spec)
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("MPIJobSpec passed validation.")
+	}
 
-    // Create in-cluster configuration object.
+	// Create in-cluster configuration object.
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
+	} else {
+		fmt.Println("Cluster config object created.")
 	}
 
-    // Obtain clientset from cluster.
-	clientSet, err := kubernetes.NewForConfig(config)
+	// Obtain clientset from cluster. We will need it to access launcher pod logs.
+    // Commenting out function call because we're not using it in code yet.
+    // clientSet, err := kubernetes.NewForConfig(config)
+	// if err != nil {
+	// 	panic(err.Error())
+	// } else {
+	// 	fmt.Println("Clientset obtained from config.")
+	// }
+
+	// Obtain client subset containing just the kubeflow based resources.
+	kubeClientSubset, err := kubeClient.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
+	} else {
+		fmt.Println("Kubeflow client subset obtained from config.")
 	}
 
-    // Obtain client subset containing just the kubeflow based resources.
-    kubeClientSubset, err := kubeClient.NewForConfig(config)
-    if err != nil {
-        panic(err.Error())
-    }
+	// Obtain an interface to the MPIJobs collection for specified namespace.
+	mpiJobs := kubeClientSubset.MPIJobs("jacek-dev")
 
-    // Obtain an interface to the MPIJobs collection for given namespace.
-    mpiJobs := kubeClientSubset.MPIJobs("jacek-dev")
+	// Submit request to create MPIJob. 
+    // It's confusing because an MPIJob is passed as an argument as well.
+    // The MPIJob being returned should have an active status.
+	_, err := mpiJobs.Create(context.TODO(), &job, meta.CreateOptions{})
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("MPIJob was successfully submitted.")
+	}
 
-    // Now attempt to submit sample MPIJob:
-    func (c *mPIJobs) Get(ctx context.Context, name string, options metav1.GetOptions) (result *v1.MPIJob, err error) 
+	results, err := mpiJobs.List(context.TODO(), meta.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("MPIJobs collection obtained.")
+	}
 
-    func (c *mPIJobs) List(ctx context.Context, opts metav1.ListOptions) (result *v1.MPIJobList, err error) {
+	// Display status of mpi jobs in collection.
+	for _, r := range results.Items {
+		fmt.Println("Job name: ", r.(meta.ObjectMeta).Name)
+		for _, c := range r.Status.Conditions {
+			fmt.Println("Condition type:", c.Type, " ... ", "Status", c.Status)
+			fmt.Println("")
+		}
+	}
 
 	// Use os.Exit to terminate the program and return a status code.
 	// In cases of error we will be able to send an error status code that the web service should pick up.
 	os.Exit(0)
 }
 
-
-// Might want to define a structure for the arguments coming from openm web service as that will
-// let us provide default values for some fields, and refactor mpiJobSpec below to accept the
-// structure instead.
-type mpiJobArgs struct {
-	modelName string
-	exeStem   string
-	dir       string
-	binDir    string
-	dbPath    string
-	mpiNp     int32
-	args      []string
-	env       map[string]string
-}
-
-// Generate mpijob spec based on arguments coming from openm web service and cluster configuration:
-func mpiJobSpec(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, args []string, env map[string]string) kubeAPI.MPIJobSpec {
+// Generate mpijob object based on arguments coming from openm web service and cluster configuration:
+func mpiJob(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, args []string, env map[string]string) kubeAPI.MPIJob {
 
 	// Start off by constructing constituent parts from the bottom up:
 	timeStamp := strconv.FormatInt(time.Now().UnixNano(), 10)
@@ -200,11 +214,12 @@ func mpiJobSpec(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, arg
 		Resources: workerResourceRequirements,
 	}
 
-	// Next is pod specs:
+	// Launcher pod specs:
 	launcherPodSpec := core.PodSpec{
 		Containers: []core.Container{launcherContainer},
 	}
 
+	// Worker pod specs:
 	workerPodSpec := core.PodSpec{
 		Containers: []core.Container{workerContainer},
 	}
@@ -219,11 +234,6 @@ func mpiJobSpec(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, arg
 		Labels: labels,
 	}
 
-	//podTypeMetadata := meta.TypeMeta{
-	//	Kind:       "PodTemplate",
-	//	APIVersion: "v1",
-	//}
-
 	// Pod template specifications:
 	launcherPodTemplateSpec := core.PodTemplateSpec{
 		ObjectMeta: podObjectMetadata,
@@ -235,8 +245,8 @@ func mpiJobSpec(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, arg
 		Spec:       workerPodSpec,
 	}
 
+	// Launcher and worker replica specs:
 	var one int32 = 1
-	// Then replica specs:
 	launcherReplicaSpec := kubeAPI.ReplicaSpec{
 		Replicas: &one,
 		Template: launcherPodTemplateSpec,
@@ -247,11 +257,12 @@ func mpiJobSpec(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, arg
 		Template: workerPodTemplateSpec,
 	}
 
+	// MPIJobSpec:
 	var two int32 = 2
 	var cleanPodPolicy kubeAPI.CleanPodPolicy = kubeAPI.CleanPodPolicyRunning
-	// Finally the mpijob spec:
 	mpiJobSpec := kubeAPI.MPIJobSpec{
-		SlotsPerWorker: &two, // probably 2 given Pat's comments about typical nodes used in the cluster.
+		// 2 slots conforms to typical worker nodes used in the aaw cluster.
+		SlotsPerWorker: &two,
 
 		// This one is a map from the set of replica types {Launcher, Worker, ... } to *ReplicaSpecs.
 		// We don't need to provide ReplicaSpecs for any replica types other than Launcher and Worker.
@@ -264,112 +275,31 @@ func mpiJobSpec(modelName, exeStem, dir, binDir, dbPath string, mpiNp int32, arg
 		MainContainer:  mainContainerName,
 		CleanPodPolicy: &cleanPodPolicy,
 	}
-	return mpiJobSpec
-}
 
-// Function based on go-client example code that queries the kubernets api for pod info:
-func showPods(cs *kubernetes.Clientset, namespace string) {
-	// Specify namespace to get pods in particular namespace
-	// or omit parameter to search all namespaces.
-	pods, err := cs.CoreV1().Pods(namespace).List(context.TODO(), meta.ListOptions{})
+	// Get hostname to annotate to MPIJob object.
+	data, err := os.ReadFile("/etc/hostname")
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	hostname := string(data)
 
-	// Use some reflection functions to explore the pods variable:
-
-	// Examples for error handling:
-	// - Use helper functions e.g. errors.IsNotFound()
-	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-	_, err = cs.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", meta.GetOptions{})
-	if errors.IsNotFound(err) {
-		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Printf("Found example-xxxxx pod in default namespace\n")
-	}
-}
-
-// Modify this to conform to our use case. We'll have already created a clientset.
-// And we will want to use a label selector or name to locate the launcher pod for a given MPIjob.
-func getPodLogs(pod core.Pod) string {
-    podLogOpts := core.PodLogOptions{}
-    config, err := rest.InClusterConfig()
-    if err != nil {
-        return "error in getting config"
+    // Type and object metadata:
+    tm := meta.TypeMeta {
+        Kind: "MPIJob",
+        APIVersion: "kubeflow.org/v1",
     }
-    // creates the clientset
-    clientset, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        return "error in getting access to K8S"
+    om := meta.ObjectMeta {
+        Name: strings.Join([]string{modelName, timeStamp}, "-"),
+		Labels: map[string]string{
+		    "notebook-name": hostname,
+		},
     }
-    req := clientset.Core().Pods(pod.Namespace).GetLogs(pod.Name, &podLogOpts)
-    podLogs, err := req.Stream()
-    if err != nil {
-        return "error in opening stream"
-    }
-    defer podLogs.Close()
 
-    buf := new(bytes.Buffer)
-    _, err = io.Copy(buf, podLogs)
-    if err != nil {
-        return "error in copy information from podLogs to buf"
-    }
-}
-
-// Keeping the reflection code here for now, but probably won't need it.
-func discoverApis(cs *kubernetes.Clientset, namespace string) {
-    // Create the meta-variable to examine the clientset variable.
-	cst := reflect.TypeOf(cs)
-    fmt.Println("The underlying type of cs: ", cst.Name())
-
-    for i := 0; i < cst.NumMethod(); i++ {
-		method := cst.Method(i)
-		name := method.Name
-		tp := method.Type
-		fmt.Println("Name: ", name)
-		fmt.Println("Type: ", tp)
-		fmt.Println()
+	// Construct MPIJob object:
+	job := kubeAPI.MPIJob{
+		(meta.TypeMeta): tm,
+		(meta.ObjectMeta): om,
+		Spec: mpiJobSpec,
 	}
-
-    // Try to call several of the most promising ones and repeat the method discovery process:
-    extensionsV1beta1 := cs.ExtensionsV1beta1()
-    extensionsV1beta1Type := reflect.TypeOf(extensionsV1beta1)
-    fmt.Println("Method set of the extensionsV1beta1 client subset:")
-    for i := 0; i < extensionsV1beta1Type.NumMethod(); i++ {
-        method := extensionsV1beta1Type.Method(i)
-        name := method.Name
-        tp := method.Type
-        fmt.Println("Name: ", name)
-        fmt.Println("Type: ", tp)
-        fmt.Println()
-	}
-
-    restClient := cs.RESTClient()
-    restClientType := reflect.TypeOf(restClient)
-    fmt.Println("Method set of the RESTClient subset:")
-    for i := 0; i < restClientType.NumMethod(); i++ {
-        method := restClientType.Method(i)
-        name := method.Name
-        tp := method.Type
-        fmt.Println("Name: ", name)
-        fmt.Println("Type: ", tp)
-        fmt.Println()
-	}
-
-    discoveryV1beta1 := cs.DiscoveryV1beta1()
-    discoveryV1beta1Type := reflect.TypeOf(discoveryV1beta1)
-    fmt.Println("Method set of the discoveryV1beta1 client subset:")
-    for i := 0; i < discoveryV1beta1Type.NumMethod(); i++ {
-        method := discoveryV1beta1Type.Method(i)
-        name := method.Name
-        tp := method.Type
-        fmt.Println("Name: ", name)
-        fmt.Println("Type: ", tp)
-        fmt.Println()
-	}
+	return job
 }
