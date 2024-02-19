e763c78c51a4d5633b2d06afe95e93241b2b6787
souheil-yazji <souheil.yaz@gmail.com>
2024-02-19 14:50:07 +0000

    chore(refactor): format + apply PR feedback

--- mpi-job-files/parseCommand.py
+++ mpi-job-files/parseCommand.py
@@ -4,24 +4,26 @@ import os
 from time import time
 from pathlib import Path
 
-# Working directory is assumed to be: 
+# Working directory is assumed to be:
 # /opt/openmpp/<openmpp-root-dir>/
 
 # Get directory where model executables are stored:
 with open("./etc/oms_model_dir") as mD:
-  modelBinsDir = os.path.join(mD.read().strip("\n"), "bin".strip("\n"))
+    modelBinsDir = os.path.join(mD.read().strip("\n"), "bin".strip("\n"))
 
 # Load manifest template contents:
 with open("./etc/MPIJobTemplate.yaml") as template:
-  manifest = template.read()
+    manifest = template.read()
 
 # Save input arguments to file for debugging:
 with open("./etc/inputArguments", "w") as inputArgs:
-  inputArgs.write(' '.join(sys.argv))
+    # merge argv array into a single string
+    argString = " ".join(sys.argv)
+    inputArgs.write(argString)
 
 # note, absolute path, no dot
 with open("/etc/hostname") as nN:
-  notebookName = nN.read()
+    notebookName = nN.read()
 
 # Save unrecognized command line options to file for debugging:
 unrecognized = ""
@@ -31,77 +33,81 @@ unrecognized = ""
 manifest = manifest.replace("#<notebookName>", notebookName)
 
 # Set working directory for mpirun to the ...models/bin directory:
-manifest = manifest.replace("#<mpirunOption>", \
-  f"- -wdir\n{12*' '}- {modelBinsDir}\n{12*' '}#<mpirunOption>")
+manifest = manifest.replace(
+    "#<mpirunOption>", f"- -wdir\n{12*' '}- {modelBinsDir}\n{12*' '}#<mpirunOption>"
+)
 
-#These variables need to be scoped outside of the while loop:
+# These variables need to be scoped outside of the while loop:
 openmOptions = []
 modelExecutable = ""
 
 ############################
 # KLW 2024-02-07 replace command line argument parsing
-#input string (command line arguments) handling
-# merge argv array into a single string
-inputArgsString = ' '.join(sys.argv)
+# input string (command line arguments) handling
 # split on ' -' instead of just space
-theArgs = inputArgsString.split(" -")
-
+argArray = argString.split(" -")
 argDict = {}
 
-#arg[0] is the command itself, so ignore
-for i in range(1, len(theArgs)):
-  argItem = theArgs[i].split() # defaults to spliting on space
-  argKey = "-" + argItem[0] # replace the - at the beginning
-  argVal = ""
-  for j in range(1,len(argItem)):
-    argVal = argVal + argItem[j] # combine the rest of the argItem, removing spaces
-    
-  #finally, add to the Dict
-  argDict[argKey] = argVal.strip()
-  #print(argKey, " ", argVal)
+# arg[0] is the command itself, so ignore
+for i in range(1, len(argArray)):
+    argItem = argArray[i].split()  # defaults to spliting on space
+    argKey = "-" + argItem[0]  # replace the - at the beginning
+    argVal = ""
+    for j in range(1, len(argItem)):
+        argVal = argVal + argItem[j]  # combine the rest of the argItem, removing spaces
+
+    # finally, add to the Dict
+    argDict[argKey] = argVal.strip()
+    # print(argKey, " ", argVal)
 
 # add the command for completeness
-argDict["command"] = theArgs[0]
+argDict["command"] = argArray[0]
 
 ### Modify the manifest template
 mv = argDict["-modelName"]
-modelExecutable = '_'.join([os.path.join(modelBinsDir, argDict["-modelName"]), "mpi"])
+modelExecutable = "_".join([os.path.join(modelBinsDir, mv), "mpi"])
 
 # KLW 16-01-2024 https://github.com/StatCan/openmpp/issues/51
 if not os.path.isfile(modelExecutable):
-  print("ERROR. Model executable not found: ", modelExecutable)
-  exit(1)
+    print("ERROR. Model executable not found: ", modelExecutable)
+    exit(1)
 
 mpiJobName = f"{mv}-{str(time()).replace('.', '-')}".lower()
 
 
 with open("./etc/mpiJobName", "w") as jN:
-  jN.write(mpiJobName)
+    jN.write(mpiJobName)
 
 manifest = manifest.replace("#<mpiJobName>", mpiJobName)
 
 mv = argDict["-mpiNp"]
-manifest = manifest.replace("#<numberOfReplicas>", f"{mv}") #argDict["-mpiNp"]) 
+manifest = manifest.replace("#<numberOfReplicas>", f"{mv}")  # argDict["-mpiNp"])
 
-manifest = manifest.replace("#<mpirunOption>", f"- -n\n{12*' '}- '{mv}'\n{12*' '}#<mpirunOption>")
+manifest = manifest.replace(
+    "#<mpirunOption>", f"- -n\n{12*' '}- '{mv}'\n{12*' '}#<mpirunOption>"
+)
 
 if "--bind-to" in argDict:
-  mv = argDict["--bind-to"]
-  manifest = manifest.replace("#<mpirunOption>", f"- --bind-to\n{12*' '}- {mv}\n{12*' '}#<mpirunOption>")
+    mv = argDict["--bind-to"]
+    manifest = manifest.replace(
+        "#<mpirunOption>", f"- --bind-to\n{12*' '}- {mv}\n{12*' '}#<mpirunOption>"
+    )
 
 if "-x" in argDict:
-  mv = argDict["-x"]
-  manifest = manifest.replace("#<mpirunOption>", f"- -x\n{12*' '}- {mv}\n{12*' '}#<mpirunOption>")
+    mv = argDict["-x"]
+    manifest = manifest.replace(
+        "#<mpirunOption>", f"- -x\n{12*' '}- {mv}\n{12*' '}#<mpirunOption>"
+    )
 
 # loop through the -OpenM. arguments and add them to the openmOptions string
 for key in argDict.keys():
-  if re.match("^-OpenM\.", key):
-    openmOptions.append(key)
-    if len(argDict[key]) > 0:
-      openmOptions.append(argDict[key])
-  if re.match("^-ini$", key):
-    openmOptions.append(key)
-    openmOptions.append(argDict[key])
+    if re.match("^-OpenM\.", key):
+        openmOptions.append(key)
+        if len(argDict[key]) > 0:
+            openmOptions.append(argDict[key])
+    if re.match("^-ini$", key):
+        openmOptions.append(key)
+        openmOptions.append(argDict[key])
 
 
 # Compose bash arguments and replace placeholder in mpijob template:
